fdbbb062701c3bb260be6a2e40b6c2a76fd6ec85
Tyler Dunn <robotdude@att.net>
2015-06-15 18:37:23 -0400

    cpufreq: Import Impulse and Nightmare governors
    Import latest Impulse and Nightmare CPU governors from @dorimanx.
    Nightmare excludes the mpdecision commit as we have mpdecision disabled
    from the start.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -397,6 +397,12 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BACON)
 extern struct cpufreq_governor cpufreq_gov_bacon;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bacon)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
 #endif
 
 /*********************************************************************
