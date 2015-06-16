c8ab5000cc37dc5454fff020d60b418d5c0dd563
Tyler Dunn <robotdude@att.net>
2015-06-15 20:19:16 -0400

    Nightmare: Switch to functions within kernel

--- drivers/cpufreq/cpufreq_nightmare.c
+++ drivers/cpufreq/cpufreq_nightmare.c
@@ -576,7 +576,7 @@ static void do_nightmare_timer(struct work_struct *work)
 		delay = max(delay - (jiffies % delay), usecs_to_jiffies(nightmare_tuners_ins.sampling_rate / 2));
 	}
 
-	mod_delayed_work_on(this_nightmare_cpuinfo->cpu,
+	schedule_delayed_work_on(this_nightmare_cpuinfo->cpu,
 		system_wq, &this_nightmare_cpuinfo->work, delay);
 	mutex_unlock(&this_nightmare_cpuinfo->timer_mutex);
 }
@@ -632,7 +632,7 @@ static int cpufreq_governor_nightmare(struct cpufreq_policy *policy,
 			delay = max(delay - (jiffies % delay), usecs_to_jiffies(nightmare_tuners_ins.sampling_rate / 2));
 		}
 
-		INIT_DEFERRABLE_WORK(&this_nightmare_cpuinfo->work, do_nightmare_timer);
+		INIT_DELAYED_WORK_DEFERRABLE(&this_nightmare_cpuinfo->work, do_nightmare_timer);
 		mod_delayed_work_on(cpu,
 			system_wq, &this_nightmare_cpuinfo->work, delay);
 
